3325166b2bc2e79bb89d4187a75d61258901338a
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-11 09:17:26 -0500

    wtite specs to test the add method if given a string of 2 numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,5 +21,19 @@ describe StringCalculator do
       end
     end
 
+    context "two numbers" do
+      context "given '2,4'" do
+        it "returns 6" do
+          expect(StringCalculator.add("2,4")).to eq(6)
+        end
+      end
+
+      context "given '17,100'" do
+        it "returns 117" do
+          expect(StringCalculator.add("17,100")).to eq(117)
+        end
+      end
+    end
+
   end
 end
